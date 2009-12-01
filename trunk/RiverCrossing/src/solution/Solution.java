795cbc96cdabdef730a8f7adfec1413c351a7664
ayal.warman <ayal.warman@dbf4a170-daad-11de-a65c-977ad3277207>
2009-12-01 00:30:11 +0000

    Level, BoardState classes defined.

--- trunk/RiverCrossing/src/solution/Solution.java
+++ trunk/RiverCrossing/src/solution/Solution.java
@@ -1,7 +1,22 @@
 package solution;
 
+import java.util.Vector;
+
+import game.BoardState;
+import game.Level;
+
 public class Solution {
 
-	protected boolean[][] _solMat;
+	/**
+	 * a solution is defined for a single level.
+	 */
+	protected Level _level;
+	
+	/**
+	 * a solution (legal or not) is defined as a sequence of states.
+	 * ie. sequence of plank-placing on the board.
+	 */
+	protected Vector<BoardState> _sequence;
+	
 	
 }
