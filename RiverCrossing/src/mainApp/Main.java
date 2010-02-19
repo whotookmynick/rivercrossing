1af7be4c1e4cfb25fdf2d42b9ed95a372179f0fc
noamara <noamara@dbf4a170-daad-11de-a65c-977ad3277207>
2010-02-19 18:48:26 +0000

    FINAL Version

--- RiverCrossing/src/mainApp/Main.java
+++ RiverCrossing/src/mainApp/Main.java
@@ -14,19 +14,12 @@ public class Main {
 	public static void main(String[] args) {
 				
 		LevelFactory factory = new LevelFactory();
-		Level level = factory.getLevel(9);
+		Level level = factory.getLevel(8);
 		level.print();
 		GeneralAlg.evolutionaryRiverCrossing(level);
-//		BoardState[][] states = GeneralAlg.generateRandomSolutions(level);
-//		for (int i=0; i<20; i++) {
-//			states[0][i].print();
-//		}	
-//		//GeneralAlg.evolutionaryRiverCrossing(level);
-//		System.out.println("******************** NEW POPULATION **************");
-//		BoardState[][] newStates = GeneralAlg.createNewPopulation(states);
-//		for (int i=0; i<20; i++) {
-//			newStates[0][i].print();
-//		}
+		
+		System.exit(0);
+
 	}
 	
 }
